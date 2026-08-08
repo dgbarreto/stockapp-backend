546d6a6909d1d39461ed85233f6c3a587155012a
Danilo Barreto <dgbarreto@gmail.com>
2026-08-08 17:48:45 -0300

    fix: Fixes #31

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    exceptionFactory: (errors) => {
+      const message = errors
+        .flatMap((error) => Object.values(error.constraints ?? {}))
+        .join('; ');
+      return new BadRequestException(message);
+    }
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
